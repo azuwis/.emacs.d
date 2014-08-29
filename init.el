68c606c5925650efc516635ed11f5888d230159c
Zhong Jianxin <azuwis@gmail.com>
2014-08-30 00:23:12 +0800

    More comments, minor setting tweak

--- init.el
+++ init.el
@@ -3,29 +3,36 @@
 ;;; Commentary:
 
 ;;; Code:
-; common settings
+;; common settings
+; no cursor blink
 (blink-cursor-mode 0)
+; no splash
 (setq inhibit-startup-message t)
-(setq scroll-step 1
-      scroll-margin 3
-      scroll-conservatively 10000)
+; scroll behavior
+(setq scroll-margin 3)
+(setq scroll-conservatively 100)
+; default major mode
 (setq major-mode 'text-mode)
+; set title to 'buffer @ file'
 (setq frame-title-format "%b @ %f")
+; show maching parenthesis
+(show-paren-mode t)
+(setq show-paren-style 'mixed)
 
-; elpa
+;; elpa
 (require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
 (package-initialize)
 
-; evil
+;; evil
 (require 'evil)
 (evil-mode t)
 
-; zenburn
+;; zenburn
 (load-theme 'zenburn t)
 
-; flycheck
+;; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
 (provide 'init)
