1731e9d4dc2fad0d278e80a40addc27dae326c25
Zhong Jianxin <azuwis@gmail.com>
2014-08-30 00:01:11 +0800

    More comments

--- init.el
+++ init.el
@@ -1,22 +1,32 @@
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+;;; Init --- Emacs init config
 
-; evil-mode
-(require 'evil)
-(evil-mode t)
+;;; Commentary:
 
-; theme
-(load-theme 'zenburn t)
-
+;;; Code:
+; common settings
 (blink-cursor-mode 0)
 (setq inhibit-startup-message t)
 (setq scroll-step 1
       scroll-margin 3
       scroll-conservatively 10000)
-(setq default-major-mode 'text-mode)
+(setq major-mode 'text-mode)
 (setq frame-title-format "%b @ %f")
+
+; elpa
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+; evil
+(require 'evil)
+(evil-mode t)
+
+; zenburn
+(load-theme 'zenburn t)
 
 ; flycheck
 (add-hook 'after-init-hook #'global-flycheck-mode)
+
+(provide 'init)
+;;; init.el ends here
